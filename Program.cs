a7cf3cbc044e74a05f01194b5f0259afaf0712bc
maljaethereal <121117752+MaksimOlienyk@users.noreply.github.com>
2025-10-19 18:59:13 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿class Iterator
+{
+    private string[] items; private int index = 0;
+    public Iterator(string[] i) { items = i; }
+    public string Next() => index < items.Length ? items[index++] : null;
+}
+
+class Program
+{
+    static void Main()
+    {
+        var it = new Iterator(new[] { "A", "B", "C" });
+        string item; while ((item = it.Next()) != null) Console.WriteLine(item);
+    }
+}
+
